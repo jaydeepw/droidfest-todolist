0890f926671c8ff4081c0c30948a717d510b45ec
Jaydeep <jaydeep.w@gmail.com>
2015-01-06 12:40:58 +0530

    add item click listener

--- TodoListApp/app/src/main/java/com/intouchapp/todolistapp/fragments/TodoListFragment.java
+++ TodoListApp/app/src/main/java/com/intouchapp/todolistapp/fragments/TodoListFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.intouchapp.todolistapp.R;
 
@@ -12,6 +14,9 @@ import com.intouchapp.todolistapp.R;
  * Created by jay on 06/01/15.
  */
 public class TodoListFragment extends Fragment {
+
+    private EditText mTodoData;
+    private View mAdd;
 
     public TodoListFragment() {
     }
@@ -21,5 +26,30 @@ public class TodoListFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mTodoData = (EditText) view.findViewById(R.id.todo_data);
+        mAdd = view.findViewById(R.id.btn_add);
+
+        initOnAddClick();
+    }
+
+    private void initOnAddClick() {
+        if(mAdd != null) {
+            mAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    addItem();
+                }
+            });
+        }
+    }
+
+    private void addItem() {
+        Toast.makeText(getActivity(), "add clicked", Toast.LENGTH_SHORT).show();
     }
 }
