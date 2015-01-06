8c0ea724c75c1f3a2115abbe13c5690ca0b3ebe1
Jaydeep <jaydeep.w@gmail.com>
2015-01-06 12:26:57 +0530

    add app with simple basic UI

---
+++ TodoListApp/app/src/main/java/com/intouchapp/todolistapp/fragments/TodoListFragment.java
@@ -0,0 +1,25 @@
+package com.intouchapp.todolistapp.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.intouchapp.todolistapp.R;
+
+/**
+ * Created by jay on 06/01/15.
+ */
+public class TodoListFragment extends Fragment {
+
+    public TodoListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
